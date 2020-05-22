196d26921c16feb781d302e3999c891d86359e65
amirhossein hamidian <amirh.hamidian@gmail.com>
2020-05-23 03:44:03 +0430

    postgresql

--- django/taakmart/settings/production.py
+++ django/taakmart/settings/production.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', "dokkan"),
+        'USER': os.environ.get('POSTGRES_USER', "amirhossein"),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ""),
+        'HOST': os.environ.get("POSTGRES_HOST", ""),
+        'PORT': "5432"
+    }
+}
